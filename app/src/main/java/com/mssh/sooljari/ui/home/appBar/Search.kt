f783d93ccd7183097eb0d76ea708bc9238a11770
Lee Jihye <jihae0584@gmail.com>
2023-09-21 05:42:23 +0900

    [FEAT] 검색바에 힌트 텍스트 추가 #1

--- app/src/main/java/com/mssh/sooljari/ui/home/appBar/Search.kt
+++ app/src/main/java/com/mssh/sooljari/ui/home/appBar/Search.kt
@@ -10,27 +10,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mssh.sooljari.R
 import com.mssh.sooljari.ui.theme.SoolJariTheme
 
 
 @Composable
 fun Search() {
-
+    SearchBar()
 }
 
 @Composable
 private fun SearchBar() {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .height(36.dp)
@@ -67,7 +75,20 @@ private fun SearchBar() {
             .width(8.dp)
         )
 
-
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = stringResource(id = R.string.searchbar_hint),
+                color = colorResource(id = R.color.neutral0),
+                fontSize = 12.sp,
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .wrapContentWidth()
+            )
+        }
     }
 }
 
