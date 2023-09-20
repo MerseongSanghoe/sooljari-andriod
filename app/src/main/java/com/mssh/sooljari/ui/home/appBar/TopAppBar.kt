51cecd82d10b9151da62dd20ff66ec5b99eca0ca
Lee Jihye <jihae0584@gmail.com>
2023-09-21 06:14:52 +0900

    [FIX] Top app bar가 적절한 크기로 그려지도록 함 #1

--- app/src/main/java/com/mssh/sooljari/ui/home/appBar/TopAppBar.kt
+++ app/src/main/java/com/mssh/sooljari/ui/home/appBar/TopAppBar.kt
@@ -1,16 +1,19 @@
 package com.mssh.sooljari.ui.home.appBar
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,17 +25,25 @@ import com.mssh.sooljari.ui.theme.SoolJariTheme
 import com.mssh.sooljari.ui.theme.logoFont
 
 @Composable
-private fun TopAppBar() {
+fun TopAppBar() {
 
-    Box {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
         Image(
             painter = painterResource(id = R.drawable.bg_main),
-            contentDescription = null)
+            contentScale = ContentScale.FillBounds,
+            contentDescription = null,
+            modifier = Modifier.matchParentSize()
+        )
 
         Column (
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
+                .wrapContentHeight()
                 .padding(12.dp)
         ) {
             Text(
