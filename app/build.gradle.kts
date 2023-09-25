39518ffdd14233afa96d15914e74bcd2052dc7c9
Lee Jihye <jihae0584@gmail.com>
2023-09-25 18:35:15 +0900

    [FEAT] status bar 투명화 #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //status bar 색 변경용
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
 }
