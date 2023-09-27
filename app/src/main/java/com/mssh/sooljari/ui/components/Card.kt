323ea068a8e0b6af83268fb0fe85c3c839b660f5
Lee Jihye <jihae0584@gmail.com>
2023-09-28 02:34:18 +0900

    [FEAT] 카드 UI 파일 생성 #9

---
+++ app/src/main/java/com/mssh/sooljari/ui/components/Card.kt
@@ -0,0 +1,18 @@
+package com.mssh.sooljari.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VerticalCard() {
+
+}
+
+@Composable
+fun HorizontalCard() {
+
+}
+
+@Composable
+fun FairCard() {
+
+}
