10b15bf13de91c228c2bb9b5ff30cd4529b986f1
Lee Jihye <jihae0584@gmail.com>
2023-09-28 02:35:31 +0900

    [FEAT] 앱 화면을 Status bar 영역까지 늘림 #1

--- app/src/main/java/com/mssh/sooljari/MainActivity.kt
+++ app/src/main/java/com/mssh/sooljari/MainActivity.kt
@@ -3,23 +3,29 @@ package com.mssh.sooljari
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.mssh.sooljari.ui.home.appBar.TopAppBar
+import com.mssh.sooljari.ui.home.navigation.NavigationBar
 import com.mssh.sooljari.ui.theme.SoolJariTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //status bar까지 화면 늘려주기
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             SoolJariTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    SoolJariApp()
                 }
             }
         }
