408487542f7a18dd01fe36cc57234ee27a0e41a1
Lee Jihye <jihae0584@gmail.com>
2023-10-03 19:54:50 +0900

    [FEAT] 결과 카드 UI 그림 #16

--- app/src/main/java/com/mssh/sooljari/ui/components/Card.kt
+++ app/src/main/java/com/mssh/sooljari/ui/components/Card.kt
@@ -1,6 +1,34 @@
 package com.mssh.sooljari.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mssh.sooljari.R
 
 @Composable
 fun VerticalCard() {
@@ -15,4 +43,88 @@ fun HorizontalCard() {
 @Composable
 fun FairCard() {
 
+}
+
+@Composable
+fun ResultCard() {
+    val cardHeight = 100.dp
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(id = R.color.neutral0)
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_placeholder),
+                modifier = Modifier
+                    .size(cardHeight)
+                    .background(
+                        color = colorResource(id = R.color.neutral3_alpha15)
+                    ),
+                contentScale = ContentScale.Fit,
+                contentDescription = null
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .wrapContentHeight()
+                ) {
+                    Text(
+                        text = "술 이름",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        text = "주종 + 도수",
+                        fontSize = 14.sp,
+                    )
+
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(8.dp)
+                    )
+
+                    ResultCardChip()
+                }
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .size(18.dp),
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Black
+                    ),
+                    contentPadding = PaddingValues(0.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_star),
+                        contentDescription = null,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ResultCardPreview() {
+    ResultCard()
 }
