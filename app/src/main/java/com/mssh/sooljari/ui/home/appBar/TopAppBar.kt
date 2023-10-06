7d8b38994e46a3362a788d7eb82de94804f173cd
Lee Jihye <jihae0584@gmail.com>
2023-10-06 15:59:48 +0900

    [REFACTOR] 태그 표시를 TagListLazyRows()로 하도록 변경

--- app/src/main/java/com/mssh/sooljari/ui/home/appBar/TopAppBar.kt
+++ app/src/main/java/com/mssh/sooljari/ui/home/appBar/TopAppBar.kt
@@ -1,7 +1,6 @@
 package com.mssh.sooljari.ui.home.appBar
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,85 +11,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mssh.sooljari.R
-import com.mssh.sooljari.ui.components.SearchBarTagChip
+import com.mssh.sooljari.ui.components.TagListLazyRows
+import com.mssh.sooljari.ui.components.searchBarTagChip
+import com.mssh.sooljari.ui.components.testTags
 import com.mssh.sooljari.ui.theme.SoolJariTheme
 import com.mssh.sooljari.ui.theme.logoFont
-
-private fun addHashTag(tagList: List<String>): List<String> {
-    val tagAddedList: MutableList<String> = ArrayList(tagList)
-
-    tagAddedList.forEachIndexed { index, tag ->
-        tagAddedList[index] = "#$tag"
-    }
-
-    return tagAddedList
-}
-
-@Composable
-fun LazyRowWithDynamicChips(
-    tagStrings: List<String> = testTags,
-    lazyRowWidth: Dp
-) {
-    val density = LocalDensity.current
-
-    //받은 태그 목록에 해시태그 글자 추가
-    val hashTaggedString: List<String> = addHashTag(tagStrings)
-    //태그 사이 간격
-    val betweenSpace: Dp = 8.dp
-
-    var currentWidth: Dp = 0.dp
-
-
-
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(betweenSpace),
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-    ) {
-        items(hashTaggedString) { tagString ->
-            val chipWidth = remember(tagString) {
-                val textWidth = with(density) {
-                    (tagString.length * 10).sp.toDp()
-                }
-                // chip 모양 때문에 발생하는 패딩 추가
-                textWidth + 24.dp
-            }
-            //chip 너비 + 태그 사이 간격
-            if (currentWidth + (chipWidth + betweenSpace) <= lazyRowWidth) {
-                SearchBarTagChip(tagString = tagString)
-                currentWidth += (chipWidth + betweenSpace)
-            } //else return@items
-        }
-    }
-}
-
-val testTags: List<String> =
-    listOf(
-        "일곱글자태그임",
-        "여섯글자태그",
-        "다섯자태그",
-        "네자태그",
-        "세글자"
-    )
-
 
 @Composable
 fun TopAppBar(
@@ -98,7 +35,7 @@ fun TopAppBar(
 ) {
     val statusBarPadding = WindowInsets.statusBars.asPaddingValues()
 
-    Box (
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
@@ -111,16 +48,17 @@ fun TopAppBar(
                 .matchParentSize()
         )
 
-        Column (
+        Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
                 .padding(12.dp)
         ) {
-            Spacer(modifier = Modifier
-                .fillMaxWidth()
-                .height(statusBarPadding.calculateTopPadding())
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(statusBarPadding.calculateTopPadding())
             )
 
             Text(
@@ -144,15 +82,15 @@ fun TopAppBar(
                     .height(12.dp)
             )
 
-            LazyRowWithDynamicChips(
-                tagStrings = testTags,
-                lazyRowWidth = 336.dp
+            TagListLazyRows(
+                tagStringList = testTags,
+                chip = searchBarTagChip,
+                paddingBetweenChips = 8.dp,
+                rowNum = 1
             )
         }
     }
 }
-
-
 
 @Preview
 @Composable
