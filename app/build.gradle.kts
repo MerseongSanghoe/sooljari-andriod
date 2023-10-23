890b00ea46c21164194e1cf472b20a55801ab031
Lee Jihye <jihae0584@gmail.com>
2023-10-23 20:38:01 +0900

    [CHORE] Android Studio Iguana 버전으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 
     //kotlinx.serialization
-    id("org.jetbrains.kotlin.plugin.serialization") version org.gradle.kotlin.dsl.embeddedKotlinVersion
+    id("org.jetbrains.kotlin.plugin.serialization") version embeddedKotlinVersion
 }
 
 android {
@@ -57,13 +57,13 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -73,10 +73,10 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //status bar 색 변경용
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
 
     //navigation bar 적용용
-    val nav_version = "2.7.3"
+    val nav_version = "2.7.4"
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
     //http 통신용, ktor 라이브러리
