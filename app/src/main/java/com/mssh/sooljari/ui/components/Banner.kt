b176154bd2337b768ae0cb61afb84b8d4c80bbd2
Lee Jihye <jihae0584@gmail.com>
2023-11-10 00:11:59 +0900

    [FEAT] 배너 UI 그림 #4

--- app/src/main/java/com/mssh/sooljari/ui/components/Banner.kt
+++ app/src/main/java/com/mssh/sooljari/ui/components/Banner.kt
@@ -1,34 +1,102 @@
 package com.mssh.sooljari.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+import com.mssh.sooljari.R
+import com.mssh.sooljari.model.Image
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalGlideComposeApi::class)
 @Composable
 fun Banner(
     modifier: Modifier = Modifier,
-    imageList: List<String>
+    imageList: List<Image>
 ) {
     val pagerState = rememberPagerState(pageCount = { imageList.size })
 
-    HorizontalPager(
-        state = pagerState,
+    Box(
         modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
     ) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) { index ->
+            GlideImage(
+                model = imageList[index].url,
+                contentDescription = null,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(280.dp),
+                contentScale = ContentScale.Fit,
+                failure = placeholder(R.drawable.img_placeholder)
+            )
+        }
 
+        //인디케이터
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) {
+                        colorResource(id = R.color.neutral0)
+                    } else {
+                        colorResource(id = R.color.neutral0_alpha15)
+                    }
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp, vertical = 8.dp)
+                        .clip(CircleShape)
+                        .background(color = color)
+                        .size(8.dp)
+                )
+            }
+        }
     }
+
+
 }
 
 @Preview
 @Composable
 fun BannerPreview() {
-    //Banner()
+    Banner(
+        Modifier,
+        listOf(
+            Image(
+                url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            ),
+            Image(
+                url = "http://211.37.148.214/uploads/product_slowly_n2_069521a90a.jpg"
+            )
+        )
+    )
 }
