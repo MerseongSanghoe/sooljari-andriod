993560f753a7e2f898d1d349027630d6da62c9ca
Merseong <esc990720@korea.ac.kr>
2023-12-10 20:49:37 +0900

    [FIX] 검색어 앞뒤 공백 제거 수정

--- app/src/main/java/com/mssh/sooljari/model/AlcoholViewModel.kt
+++ app/src/main/java/com/mssh/sooljari/model/AlcoholViewModel.kt
@@ -76,14 +76,16 @@ class AlcoholViewModel(private val repository: AlcoholRepository, private val ap
             return
         }
 
+        val searchWord = keyword.trimIndent()
+
         _isLoading.value = true
 
-        saveToHistory(keyword)
+        saveToHistory(searchWord)
 
         viewModelScope.launch {
-            reqUrl = repository.getInitialResults(keyword).reqUrl
+            reqUrl = repository.getInitialResults(searchWord).reqUrl
 
-            val result = repository.getInitialResults(keyword).result
+            val result = repository.getInitialResults(searchWord).result
             currentPage = result.page ?: 0
             totalAlcoholCount = result.count ?: 0
             _alcoholList.value = result.data
